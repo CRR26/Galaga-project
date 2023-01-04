730b29bef9643994e9dfd7c172bb15ee55f32360
CRR26 <crodriguez25@aoiths.org>
2023-01-04 09:04:24 -0500

    Initial commit

---
+++ Assets/scripts/EnemyController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+    public float changeTime = 7.0f;
+
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+ bool broken = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         timer -= Time.deltaTime;
+         if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+       Vector2 position = rigidbody2D.position;
+        
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+        }
+        
+        rigidbody2D.MovePosition(position); 
+    }
+     void OnCollisionEnter2D(Collision2D other)
+    {
+        
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+    public GameObject projectilePrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        
+    }
+    void Update()
+    {
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+
+         if(Input.GetKeyDown(KeyCode.C))
+         {
+            Launch();
+         }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        Vector2 position = rigidbody2d.position;
+
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    } 
+    
+ void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(new Vector2(0,1), 2000);
+
+    }
+    
+    
+
+}
